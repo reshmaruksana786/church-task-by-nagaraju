837c4cfbeb77bed1ca8bd3817af0f5ca1324003e
nagaraju <naga.thota99@gmail.com>
2020-01-30 17:41:21 +0530

    Initial Commit

---
+++ TecnicalTaskForrvtec/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TecnicalTaskForrvtec
+//
+//  Created by THOTA NAGARAJU on 1/30/20.
+//  Copyright © 2020 THOTA NAGARAJU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
